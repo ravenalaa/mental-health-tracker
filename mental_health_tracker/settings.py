b01a521180099862984a6363b5fdf401c587753b
ravenalaa <claudia.paskalia@gmail.com>
2024-08-28 16:25:10 +0700

    Menambahakan URL deployment

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h+^71=9)_)+wroo)gt2=#a2lt301u(id=6up0y0tehozl@2&t1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "claudia-paskalia-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
 
 # Application definition
