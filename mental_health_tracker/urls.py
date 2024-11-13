acc394ebc42355c0715ca75818512dd5e448a33f
ravenalaa <claudia.paskalia@gmail.com>
2024-11-13 15:23:13 +0700

    Penyelesaian Tutorial 8

--- mental_health_tracker/urls.py
+++ mental_health_tracker/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls')) # Routing main
+    path('', include('main.urls')), # Routing main
+    path('auth/', include('authentication.urls')),
 ]
